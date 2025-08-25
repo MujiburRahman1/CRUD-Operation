df306991955c00c02294abf41fe39394b25b0846
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-25 16:24:01 +0500

    Update Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,10 +1,19 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 function Users() {
-    const [users, setUsers] = useState([{
-        Name: "Mujib", Email: "mujib@gmail.com", Age: 22
-    }])
+    const [users, setUsers] = useState([])
+        
+   
+
+    useEffect(() => {
+        axios.get('http://localhost:3001')
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err)
+        )
+    }, [])
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
@@ -22,13 +31,13 @@ function Users() {
                     {
                         users.map((user) => {
                            return <tr>
-                                <td>{user.Name}</td>
-                                <td>{user.Email}</td>
-                                <td>{user.Age}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
                                 <td>
-                                    <Link to="/update" className='btn btn-success'>Update</Link>
+                                    <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
 
-                                    <button>Delete</button>
+                                    <button className='btn btn-danger'>Delete</button>
                                 </td>
                             </tr>
                         })
