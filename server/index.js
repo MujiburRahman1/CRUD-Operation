e250be1dd38c262c4d3f07f2a262e2196ec5d7b4
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-25 16:59:50 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ app.get('/', (req, res) =>{
 
 app.get('/getUser/:id', (req, res) => {
     const id = req.params.id;
-    UserModel.findById({id})
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req,res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {name: req.body.name, email: req.body.email, age: req.body.age})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
