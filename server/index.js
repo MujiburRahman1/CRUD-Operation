784b8be6c7bbce3454b8c9bfe7feadf025410829
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-25 16:24:08 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,19 @@ app.use(express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/crud')
 
+app.get('/', (req, res) =>{
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
 app.post("/createUser", (req, res) => {
     UserModel.create(req.body)
     .then(users => res.json(users))
